7bb3a4257d42094deb3bfd108162f1a590ba9581
Galder <Galder@DESKTOP-EU1JRUL>
2020-04-05 10:32:01 +0200

    y78y

--- ProAlumno/src/paqalumno/Alumnos.java
+++ ProAlumno/src/paqalumno/Alumnos.java
@@ -5,7 +5,7 @@ public class Alumnos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
-		System.out.println("Adios Mundo");
+		System.out.println("Adios Mundou");
 	}
 
 }

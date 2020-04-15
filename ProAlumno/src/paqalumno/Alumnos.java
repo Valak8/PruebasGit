7ddf57d39ffbc44d4211ffe24a2b0e938f147223
Galder <Galder@DESKTOP-EU1JRUL>
2020-04-15 18:15:57 +0200

    f

--- ProAlumno/src/paqalumno/Alumnos.java
+++ ProAlumno/src/paqalumno/Alumnos.java
@@ -8,7 +8,7 @@ public class Alumnos {
 		System.out.println("Adios Mundou");
 		System.out.println("Cambio usuario1");
 		System.out.println("Cambio usuario2");
-		System.out.println("commit branchprueba1");
+		System.out.println("commit branchpruebda1");
 	}
 
 }
